7307a8376d9d83cda42ef48d1678d30fd2f419cd
abibilonilombardi <abibiloni@fi.uba.ar>
2023-10-21 15:05:42 -0300

    TCK #6 add get by product name

--- changuito-smart/src/handlers/products.ts
+++ changuito-smart/src/handlers/products.ts
@@ -1,20 +1,4 @@
-// import { getProducts } from './products'
 import { FastifyReply, FastifyRequest } from 'fastify'
-
-// export class ProductHandler {
-//   private productRepository
-
-//   constructor(dependencies) {
-//     this.productRepository = dependencies.repository
-//   }
-
-//   public async get(request: FastifyRequest, _reply: FastifyReply) {
-//     //@ts-ignore
-//     const { name } = request.query
-//     const result = await this.productRepository.findByName(name)
-//     return { product: result }
-//   }
-// }
 
 export const ProductHandler = (dependencies: any) => {
   const { product: productRepository } = dependencies
