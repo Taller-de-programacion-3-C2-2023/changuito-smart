d7ed16a47ee98556145997d5f5e5577ff791c8e2
abibilonilombardi <abibiloni@fi.uba.ar>
2023-10-22 21:09:31 -0300

    TCK #6 add cors config

--- changuito-smart/src/app.ts
+++ changuito-smart/src/app.ts
@@ -1,11 +1,13 @@
 import Fastify from 'fastify'
 import { routes } from './routes'
-import { mongoConfig } from './config'
+import { corsConfig, mongoConfig } from './config'
 import { swagger, swaggerUiOptions } from './swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
 
 export const app = (opts = {}) => {
   const app = Fastify(opts)
+  app.register(require('@fastify/cors'), corsConfig)
+
   app.register(require('@fastify/swagger'), swagger)
   app.register(fastifySwaggerUi, swaggerUiOptions)
 
--- changuito-smart/src/config/index.ts
+++ changuito-smart/src/config/index.ts
@@ -3,6 +3,19 @@ export const appConfig = {
   host: process.env.APP_IP, // || '127.0.0.1',
 }
 
+export const corsConfig = (_instance?) => (req, callback) => {
+  const corsOptions = {
+    // This is NOT recommended for production as it enables reflection exploits
+    origin: true,
+  }
+  // do not include CORS headers for requests from localhost
+  if (/^localhost$/m.test(req.headers.origin)) {
+    corsOptions.origin = false
+  }
+  // callback expects two parameters: error and options
+  callback(null, corsOptions)
+}
+
 export const mongoConfig = {
   url: 'mongodb://changuito:smart@localhost:27017/',
   forceClose: true,
