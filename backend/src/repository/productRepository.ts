e21dfddb8a924effb43804027ec3a968f28110ba
Matías Lafroce <mlafroce@gmail.com>
2024-01-28 22:18:02 -0300

    Adds pagination to products

--- backend/src/repository/productRepository.ts
+++ backend/src/repository/productRepository.ts
@@ -5,9 +5,9 @@ export default class ProductRepository extends MongoRepository {
     super(dbClient, 'db-changuito', 'products')
   }
 
-  public async findByName(name: string) {
+  public async findByName(name: string, pagination?: { limit: number; offset: number; sort?: any }) {
     const filter = { name: { $regex: `${name}`, $options: 'si' } }
-    const result = await this.find(filter)
+    const result = await this.find(filter, pagination)
     return result
   }
 }
