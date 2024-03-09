245363847ec18de71b4a3b49300a9113fdabacce
Matías Lafroce <mlafroce@gmail.com>
2024-03-09 18:23:14 -0300

    Adds fromDate & toDate to price evolution chart

---
+++ front/src/components/dateFilter.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { Calendar } from 'primereact/calendar';
+
+export default function DateFilter(props) {
+  const today = new Date();
+  const lastWeek = new Date()
+  lastWeek.setDate(today.getDate() - 7);
+
+  const [dates, setDates] = useState([lastWeek, today]);
+
+  function rangeChanged(range) {
+    setDates(range);
+    if (range[1]) {
+      props.onDateChanged(range);
+    }
+  }
+
+  return (<div>
+    <p> Rango de fechas </p>
+    <Calendar value={dates} onChange={(e) => rangeChanged(e.value)} selectionMode="range" readOnlyInput />
+    </div>);
+}
