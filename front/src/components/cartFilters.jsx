d1fcc201bc971866855eff9eb927e8aaf7c32bc0
abibilonilombardi <abibiloni@fi.uba.ar>
2024-03-09 19:20:34 -0300

    feat: add products quantity

--- front/src/components/cartFilters.jsx
+++ front/src/components/cartFilters.jsx
@@ -37,7 +37,6 @@ export default function CartFilters(props) {
         //     "brand": "NUCETE"
         //   }
         // ]
-        setProducts(json.slice(0, 10));
       }
       try {
         fetchOptions();
