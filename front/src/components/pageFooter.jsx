fc1d85da0cfbc766b652c2d00222b1d764e0fa9c
abibilonilombardi <abibiloni@fi.uba.ar>
2024-03-18 16:55:53 -0300

    feat: footer style

--- front/src/components/pageFooter.jsx
+++ front/src/components/pageFooter.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Config from "../config.js";
+import logo from "../images/fiuba-logo.png";
 
 export default function Footer(props) {
   const [scrapDate, setScrapDate] = useState("...");
@@ -24,13 +25,13 @@ export default function Footer(props) {
     // <div className="Footer">
     <footer className="Footer">
       <div className="FooterContent">
-        <img src="tu_imagen.png" alt="Logo" className="Logo" />
+        <img src={logo} alt="Logo" className="Logo" />
         <div>
           <div>
-          <p>Este es un trabajo práctico para la facultad.</p>
+            <p>Facultad de Ingeniería de la Univerisidad de Buenos Aires.</p>
           </div>
           <div>
-          <p>Ultima actualización de precios: {scrapDate}</p>
+          <p>Última actualización de precios: {scrapDate}</p>
           </div>
         </div>
       </div>
