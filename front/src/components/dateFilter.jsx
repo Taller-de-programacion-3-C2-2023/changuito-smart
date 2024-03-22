b6aaedd4a363749e8a63fd2e30d26868493105e8
abibilonilombardi <31183833+abibilonilombardi@users.noreply.github.com>
2024-03-22 16:30:03 -0300

    fix: calendar lenguaje (#57)

--- front/src/components/dateFilter.jsx
+++ front/src/components/dateFilter.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
-import { Calendar } from 'primereact/calendar';
+import { Calendar } from "primereact/calendar";
+import { addLocale } from "primereact/api";
+import { es } from "./helpers/es";
 
 export default function DateFilter(props) {
   const today = new Date();
-  const lastWeek = new Date()
+  const lastWeek = new Date();
   lastWeek.setDate(today.getDate() - 7);
 
   const [dates, setDates] = useState([lastWeek, today]);
+  addLocale("es", es);
 
   function rangeChanged(range) {
     setDates(range);
@@ -15,8 +18,21 @@ export default function DateFilter(props) {
     }
   }
 
-  return (<div className="Container">
-    <p> Rango de fechas </p>
-    <Calendar value={dates} onChange={(e) => rangeChanged(e.value)} selectionMode="range" readOnlyInput />
-    </div>);
+  return (
+    <div className="Container ">
+      <div className="flex-auto">
+          <label htmlFor="buttondisplay" className="font-bold block mb-3">
+            <h3>Rango de fechas</h3>
+          </label>
+        <Calendar
+          value={dates}
+          onChange={(e) => rangeChanged(e.value)}
+          selectionMode="range"
+          readOnlyInput
+          locale="es"
+          showIcon
+        />
+      </div>
+    </div>
+  );
 }
